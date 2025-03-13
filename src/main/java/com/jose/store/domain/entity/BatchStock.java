8ab70e9dd9fe3cef215785f6c2396b6c8c404846
Dakson38 <daksonvibes38@gmail.com>
2025-03-13 11:46:18 -0400

    feat: adding eureka client, fixing entities ids and fixing jwt error

--- src/main/java/com/jose/store/domain/entity/BatchStock.java
+++ src/main/java/com/jose/store/domain/entity/BatchStock.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.io.Serializable;
@@ -13,7 +14,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
--- src/main/java/com/jose/store/domain/entity/Provider.java
+++ src/main/java/com/jose/store/domain/entity/Provider.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.io.Serializable;
 import java.util.List;
@@ -13,7 +14,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.data.annotation.Id;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
