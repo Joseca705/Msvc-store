9a2a3651867a5b07485548704330b7e1fa140305
Dakson38 <daksonvibes38@gmail.com>
2025-03-13 20:24:55 -0400

    fix: product controller path

--- src/main/java/com/jose/store/api/controller/ProductController.java
+++ src/main/java/com/jose/store/api/controller/ProductController.java
@@ -6,11 +6,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController(value = "/product")
+@RestController
+@RequestMapping("/product")
 public class ProductController {
 
   private final IProductService productService;
